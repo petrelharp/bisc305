376bdf774eb97c96f5c434b18edcbf226187b32f
peter <petrel.harp@gmail.com>
2015-03-24 10:25:20 -0700

    .

--- bisc305-lecture15.R
+++ bisc305-lecture15.R
@@ -22,11 +22,12 @@ mean(disc.pvals < 0.05)
 
 
 
+
 # biased
 asym.pvals <- replicate(1000, {
         n <- m <- 100
         sample.1 <- c(1,10)[1+rbinom(n,size=1,prob=1/2)]
-        sample.2 <- c(6,7)[1+rbinom(m,size=1,prob=1/2)]
+        sample.2 <- c(8,9)[1+rbinom(m,size=1,prob=1/2)]
         wilcox.test(sample.1,sample.2)$p.value
     } )
 
