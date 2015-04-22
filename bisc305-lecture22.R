2fa2c1dda75267a2af680018505dfc547b8c7387
peter <petrel.harp@gmail.com>
2015-04-21 20:38:20 -0700

    .

--- bisc305-lecture22.R
+++ bisc305-lecture22.R
@@ -20,3 +20,19 @@ par(mar=c(4,4,0,0)+.1,mgp=c(2.1,1,0))
 plot( scale(N18) ~ scale(prevalence), data=toxo, ylab="neuroticism", xlab="prevalence of toxoplasmosis" )
 abline(coef( lm(scale(N18) ~ scale(prevalence), data=toxo) ) )
 dev.off()
+
+## snakes example
+
+xy <- data.frame(
+        x=c(60,69,66,64,54,67,59,65,63),
+        y=c(136,198,194,140,93,172,116,174,145)
+    )
+xylm <- lm(y~x,data=xy)
+
+pdf(file="snakes-cor-ex.pdf",width=3,height=2.5)
+par(mar=c(4,4,0,0)+.1,mgp=c(2.1,1,0))
+plot(xy,xlab="weight",ylab="length")
+abline(coef(xylm))
+text(55,190,labels=paste("y=",round(coef(xylm)[2],0),"x",round(coef(xylm)[1],0)))
+text(55,170,labels=paste("r=", round(cor(xy$x,xy$y),2) ) )
+dev.off()
